9b341c61471982e488474f784bc6f0536dfa29c9
cicm <camilocorreall44@gmail.com>
2026-03-22 18:20:59 -0400

    Initial Commit

---
+++ Iridium/IridiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  IridiumApp.swift
+//  Iridium
+//
+//  Created by Camilo on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct IridiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IridiumTests/IridiumTests.swift
@@ -0,0 +1,17 @@
+//
+//  IridiumTests.swift
+//  IridiumTests
+//
+//  Created by Camilo on 3/22/26.
+//
+
+import Testing
+@testable import Iridium
+
+struct IridiumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
